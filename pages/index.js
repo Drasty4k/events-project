df51c5d53d2aee79d167cf2c80c5d47c53830504
Dragos Barbuta <barbuta.dragos12@gmail.com>
2022-08-26 12:46:55 +0300

    navigating to the "Filtered Events" Page programmatically

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,11 @@
 import { getFeaturedEvents } from "../dummy-data";
 import EventList from "../components/events/event-list";
-import EventsSearch from "../components/events/events-search";
 
 function HomePage() {
   const featuredEvents = getFeaturedEvents();
 
   return (
     <>
-      <EventsSearch />
       <EventList items={featuredEvents} />
     </>
   );
