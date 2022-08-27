20cb0da090072b442ef040e9ee75b19d52a0cdbf
Dragos Barbuta <barbuta.dragos12@gmail.com>
2022-08-27 15:05:22 +0300

    imported Fragment

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,14 @@
 import { getFeaturedEvents } from "../dummy-data";
 import EventList from "../components/events/event-list";
+import { Fragment } from "react";
 
 function HomePage() {
   const featuredEvents = getFeaturedEvents();
 
   return (
-    <>
+    <Fragment>
       <EventList items={featuredEvents} />
-    </>
+    </Fragment>
   );
 }
 
