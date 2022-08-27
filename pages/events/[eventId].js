650bad5ec893ce50f9572068f5b176e6ea53e433
Dragos Barbuta <barbuta.dragos12@gmail.com>
2022-08-27 17:40:38 +0300

    optimized the /events page by generating a new static page every 1800 seconds (in case of new data) and the /events/[eventId] page by generating a new static page every 30 seconds and with a fallback set to true (to a have a loading state in case you want to acces an id that is not static generated)

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,6 +1,6 @@
 import { Fragment } from "react";
 
-import { getEventById, getAllEvents } from "../../helpers/api-util";
+import { getEventById, getFeaturedEvents } from "../../helpers/api-util";
 import EventSummary from "../../components/event-detail/event-summary";
 import EventLogistics from "../../components/event-detail/event-logistics";
 import EventContent from "../../components/event-detail/event-content";
@@ -12,9 +12,9 @@ function EventDetailPage(props) {
 
   if (!event) {
     return (
-      <ErrorAlert>
-        <p>No event found!</p>
-      </ErrorAlert>
+      <div className="center">
+        <p>Loading...</p>
+      </div>
     );
   }
 
@@ -43,18 +43,19 @@ export async function getStaticProps(context) {
     props: {
       selectedEvent: event,
     },
+    revalidate: 30
   };
 }
 
 export async function getStaticPaths() {
-  const events = await getAllEvents();
+  const events = await getFeaturedEvents();
 
   const paths = events.map((event) => ({ params: { eventId: event.id } }));
   console.log(paths);
 
   return {
     paths: paths,
-    fallback: false,
+    fallback: true,
   };
 }
 
