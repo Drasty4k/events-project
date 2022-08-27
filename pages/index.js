7b33b9ba5f0762c68c47e809ffcd7ca027bef204
Dragos Barbuta <barbuta.dragos12@gmail.com>
2022-08-27 16:30:25 +0300

    added Static Site Generation (SSR) on the Home page

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,24 @@
-import { getFeaturedEvents } from "../dummy-data";
+import { getFeaturedEvents } from "../helpers/api-util";
 import EventList from "../components/events/event-list";
 import { Fragment } from "react";
 
-function HomePage() {
-  const featuredEvents = getFeaturedEvents();
+function HomePage(props) {
 
   return (
     <Fragment>
-      <EventList items={featuredEvents} />
+      <EventList items={props.events} />
     </Fragment>
   );
+}
+
+export async function getStaticProps() {
+  const featuredEvents = await getFeaturedEvents();
+
+  return {
+    props: {
+      events: featuredEvents,
+    },
+  };
 }
 
 export default HomePage;
