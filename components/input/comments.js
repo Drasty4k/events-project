f5daf95090b5103bb8503008f30ab732c8cc1d1b
Dragos Barbuta <barbuta.dragos12@gmail.com>
2022-08-29 17:01:52 +0300

    triggered and show a notification if the response is a success or if its failed for the comments feature

--- components/input/comments.js
+++ components/input/comments.js
@@ -1,11 +1,14 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 
 import CommentList from "./comment-list";
 import NewComment from "./new-comment";
+import NotificationContext from "../../store/notification-context";
 import classes from "./comments.module.css";
 
 function Comments(props) {
   const { eventId } = props;
+
+  const notificationCtx = useContext(NotificationContext);
 
   const [showComments, setShowComments] = useState(false);
   const [comments, setComments] = useState([]);
@@ -23,6 +26,12 @@ function Comments(props) {
   }
 
   function addCommentHandler(commentData) {
+    notificationCtx.showNotification({
+      title: "Sending comment...",
+      message: "Your comment is currently being stored into a database",
+      status: "pending",
+    });
+
     fetch(`/api/comments/${eventId}`, {
       method: "POST",
       body: JSON.stringify(commentData),
@@ -30,8 +39,30 @@ function Comments(props) {
         "Content-Type": "application/json",
       },
     })
-      .then((response) => response.json())
-      .then((data) => console.log(data));
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+
+        return response.json().then((data) => {
+          throw new Error(data.message || "Something went wrong!");
+        });
+      })
+      .then((data) => {
+        notificationCtx.showNotification({
+          title: "Success!",
+          message: "Your comment was saved!",
+          status: "success",
+        });
+        console.log(data);
+      })
+      .catch((error) => {
+        notificationCtx.showNotification({
+          title: "Error!!",
+          message: error.message || "Something went wrong!",
+          status: "error",
+        });
+      });
   }
 
   return (
