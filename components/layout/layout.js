dc39c0b5fcfba9e8d1df8d045e71beb6d76994b9
Dragos Barbuta <barbuta.dragos12@gmail.com>
2022-08-29 16:10:26 +0300

    moved the Notification component in the Layout component to have access to the context and used the context to show and populate the Notification component

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -1,11 +1,26 @@
+import { Fragment, useContext } from "react";
+
 import MainHeader from "./main-header";
+import Notification from "../ui//notification";
+import NotificationContext from "../../store/notification-context";
 
 function Layout(props) {
+  const noficiationCtx = useContext(NotificationContext);
+
+  const activeNotification = noficiationCtx.notification;
+
   return (
-    <>
+    <Fragment>
       <MainHeader />
       <main>{props.children}</main>
-    </>
+      {activeNotification && (
+        <Notification
+          title={activeNotification.title}
+          message={activeNotification.message}
+          status={activeNotification.status}
+        />
+      )}
+    </Fragment>
   );
 }
 
